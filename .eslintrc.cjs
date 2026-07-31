e30263b6bba15fccd43d4511f8d8d6fa9b4ca33a
Adilet <publicsierra1337@gmail.com>
2026-07-31 15:37:27 +0500

    fix: allow destructured defaults for react/require-default-props

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,5 +27,9 @@ module.exports = {
     'max-lines-per-function': ['error', { max: 40, skipBlankLines: true, skipComments: true }],
     'no-param-reassign': ['error', { props: true, ignorePropertyModificationsFor: ['state'] }],
     'import/extensions': 'off',
+    'react/require-default-props': ['error', {
+      forbidDefaultForRequired: true,
+      ignoreFunctionalComponents: true,
+    }],
   },
 };
